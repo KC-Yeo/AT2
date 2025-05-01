8bf6bee3aedbdd42c1fb40612b34ed5f90906ede
KC-Yeo <yeokachen@gmail.com>
2025-05-01 23:06:09 +1000

    chaos

--- public/AT2_3.js
+++ public/AT2_3.js
@@ -1,6 +1,10 @@
 let particles = [];
 const nums = 1000;
 
+let space = 50;
+let x = 0;
+let y = 0;
+
 const noiseScale = 0.01;
 
 class Particle {
@@ -45,6 +49,37 @@ function draw() {
       p.update();
       p.display();
    }
+
+   stroke(255)
+   // strokeWeight(0.1);
+   if (random(1) < 0.1) {
+      stroke(random(255));
+      line(x, y, x + space, y + space);
+      text(x, y);
+   } else if (random(1) < 0.03) {
+      fill(random(255));
+      rect(x, y, x + space, y + space);
+      stroke(random(255));
+      text(x, y);
+      stroke(random(255));
+   } else {
+      stroke(random(255));
+      rect(x, y, space, space);
+      text(x, y);
+      stroke(random(255));
+   }
+
+   x += 20;
+
+   if (x > width) {
+      x = 0;
+      y = y + space;
+   }
+
+   if (y > height) {
+      y = 0;
+      x = x + space
+   }
 }
 
 class Particles {
@@ -72,19 +107,13 @@ class Particles {
    }
 }
 
-function glitch() {
-   textSize(8);
-   text(("error"), this.x, this.y)
+function mousePressed() {
+   noiseSeed(millis());
 }
 
 function rand_col() {
    // colorMode(HSB);
-   return color(random(200), 80, 100, 180);
-}
-
-function mousePressed() {
-   noiseSeed(millis());
-
+   return color(random(360), 80, 100, 180);
 }
 
 function onScreen(v) {  
